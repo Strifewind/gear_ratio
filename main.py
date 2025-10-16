fd59746c6dbc4288e3a8874102e218c30b0dfb71
Brian O'Grady <loudmouthbrian@gmail.com>
2025-10-16 01:58:25 -0700

    Added docstrings for functions.

--- main.py
+++ main.py
@@ -28,35 +28,97 @@ def main():
 
 
 def print_intro():
+    """
+    Prints the introduction to the program.
+    :param: none
+    :return: none
+    """
+
     print("Welcome to the bicycle gear ratio calculator!\n")
 
 
 def get_bike_id():
+    """
+    Prompts for the bike ID used to identify the bicycle.
+    :param: none
+    :return: bike id entered by the user, string
+    """
+
     bike_id = input("Enter a unique bike ID: ")
     return bike_id
 
 
 def get_chainring_teeth():
+    """
+    Prompts for the number of teeth on the front chainring.
+    :param: none
+    :return: number of chainring teeth, int
+    """
+
     chainring = int(input("Enter the number of teeth on the chainring: "))
     return chainring
 
 
 def get_cog_teeth():
+    """
+    Prompts for the number of teeth on the rear cog.
+    :param: none
+    :return: number of cog teeth, int
+    """
+
     cog = int(input("Enter the number of teeth on the cog: "))
     return cog
 
 
 def calculate_gear_ratio(chainring, cog):
+    """
+    Compute the gear ratio between chainring and cog.
+
+    The gear ratio is defined as chainring divided by cog.
+
+    Parameters
+    ----------
+    chainring : int or float
+        Number of teeth on the front chainring.
+    cog : int or float
+        Number of teeth on the rear cog. Must be non-zero.
+
+    Returns
+    -------
+    float
+        The gear ratio (chainring / cog).
+
+    Raises
+    ------
+    ZeroDivisionError
+        If ``cog`` is zero.
+    """
+
     gear_ratio = chainring / cog
     return gear_ratio
 
 
 def print_bike_info(bike_id, chainring, cog, gear_ratio):
+    """
+    Prints formatted bike information and the computed gear ratio.
+    :param bike_id: the bike identifier (string)
+    :param chainring: number of teeth on the front chainring (int)
+    :param cog: number of teeth on the rear cog (int)
+    :param gear_ratio: computed gear ratio (float)
+    :return: none
+    """
+
     print("\nBike ID - Chainring - Cog - Gear ratio")
     print(f"{bike_id}   |   {chainring}   |   {cog}   |   {gear_ratio:.2f}")
 
 
 def print_outro():
+    """
+    Prints a short closing message to the user.
+    :param: none
+    :return: none
+    """
+
     print("\nThank you for using the bicycle gear ratio calculator!")
 
 main()
